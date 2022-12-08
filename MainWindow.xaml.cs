ea49271b6856d7f433420faa4476c101e872dc09
ИСП-31 <-31@LAB17-12>
2022-12-08 13:19:07 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Triad triad1 = new Triad();
+        Triad triad2 = new Triad();
+        Triad triad3 = new Triad();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SumOfTriads_Click(object sender, RoutedEventArgs e)
+        {
+            GetTriadsValue();
+            triad3 = triad1.SumOfNum(triad1, triad2);
+            MessageBox.Show($"Новая тройка:({triad1.One} + {triad2.One}, {triad1.Two} + {triad2.Two}, {triad1.Three} + {triad2.Three}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void GetTriadsValue()
+        {
+            int.TryParse(valueOneTriad1.Text, out int valOneTriad1);
+            int.TryParse(valueTwoTriad1.Text, out int valTwoTriad1);
+            int.TryParse(valueThreeTriad1.Text, out int valThreeTriad1);
+
+            triad1.One = valOneTriad1;
+            triad1.Two = valTwoTriad1;
+            triad1.Three = valThreeTriad1;
+
+            int.TryParse(valueOneTriad2.Text, out int valOneTriad2);
+            int.TryParse(valueTwoTriad2.Text, out int valTwoTriad2);
+            int.TryParse(valueThreeTriad2.Text, out int valThreeTriad2);
+
+            triad2.One = valOneTriad2;
+            triad2.Two = valTwoTriad2;
+            triad2.Three = valThreeTriad2;
+        }
+
+        private void PowOfNumUsr_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(UsrValue.Text, out int valUsrValue))
+            {
+                GetTriadsValue();
+                triad3 = triad1.PowOfNum(valUsrValue, triad1);
+                MessageBox.Show($"Первая тройка:({triad1.One} * {valUsrValue}, {triad1.Two} * {valUsrValue}, {triad1.Three} * {valUsrValue}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+                triad3 = triad2.PowOfNum(valUsrValue, triad2);
+                MessageBox.Show($"Вторая тройка:({triad2.One} * {valUsrValue}, {triad2.Two} * {valUsrValue}, {triad2.Three} * {valUsrValue}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Значение не может быть пустым");
+                UsrValue.Focus();
+            }
+        }
+
+        private void SumOfNumUsr_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(UsrValue.Text, out int valUsrValue))
+            {
+                GetTriadsValue();
+                triad3 = triad1.SumOfNum(valUsrValue, triad1);
+                MessageBox.Show($"Первая тройка:({triad1.One} + {valUsrValue}, {triad1.Two} + {valUsrValue}, {triad1.Three} + {valUsrValue}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+                triad3 = triad2.SumOfNum(valUsrValue, triad2);
+                MessageBox.Show($"Вторая тройка:({triad2.One} + {valUsrValue}, {triad2.Two} + {valUsrValue}, {triad2.Three} + {valUsrValue}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Значение не может быть пустым");
+                UsrValue.Focus();
+            }
+        }
+
+        private void CompareTriads_Click(object sender, RoutedEventArgs e)
+        {
+            GetTriadsValue();
+            if (triad1.CompareTriad(triad1, triad2))
+            {
+                MessageBox.Show("Двойки равны");
+            }
+            else
+            {
+                MessageBox.Show("Двойки не равны");
+            }
+        }
+
+        private void TaskBut_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработчик: Агеев Алексей\nЗадание: . Использовать класс Triad (тройка чисел). Разработать операцию для сложения \nтриады с числом. Разработать операцию для сложения элементов одой триады с \nдругой триадой.", "Вариант 12", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void AppClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ClearAll_Click(object sender, RoutedEventArgs e)
+        {
+            UsrValue.Clear();
+            valueOneTriad1.Clear();
+            valueTwoTriad1.Clear();
+            valueThreeTriad1.Clear();
+            valueOneTriad2.Clear();
+            valueTwoTriad2.Clear();
+            valueThreeTriad2.Clear();
+            UsrValue.Focus();
+        }
+
+        private void SumOfUserTriadWithOperator_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(UsrValue.Text, out int userVal))
+            {
+                GetTriadsValue();
+
+                triad3 = triad1 + userVal;
+                MessageBox.Show($"Первая тройка:({triad1.One} + {userVal}, {triad1.Two} + {userVal}, {triad1.Three} + {userVal}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                triad3 = triad2 + userVal;
+                MessageBox.Show($"Вторая тройка:({triad2.One} + {userVal}, {triad2.Two} + {userVal}, {triad2.Three} + {userVal}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            else
+            {
+                MessageBox.Show("Значение не может быть пустым");
+                UsrValue.Focus();
+            }
+        }
+
+        private void SumOfTriadsWithOperator_Click(object sender, RoutedEventArgs e)
+        {
+            GetTriadsValue();
+            triad3 = triad1 + triad2;
+            MessageBox.Show($"Первая тройка:({triad1.One} + {triad2.One}, {triad1.Two} + {triad2.Two}, {triad1.Three} + {triad2.Three}) = ({triad3.One}, {triad3.Two}, {triad3.Three})", "Результат", MessageBoxButton.OK, MessageBoxImage.Information);
+
+        }
+    }
+}
---
+++ Triad.cs
@@ -0,0 +1,101 @@
+﻿namespace WpfApp1
+{
+    internal class Triad
+    {
+        /*Начало полей класса Triad с модификатором protected*/
+
+        protected int _one, _two, _three;
+
+        /*Конец полей класса Triad с модификатором protected*/
+
+        /*Начало свойств класса Triad*/
+
+        public int One { get { return _one; } set { _one = value; } }
+        public int Two { get { return _two; } set { _two = value; } }
+        public int Three { get { return _three; } set { _three = value; } }
+
+        /*Конец свойств класса Triad*/
+
+        /// <summary>
+        /// Сравнивает две тройки между собой
+        /// </summary>
+        /// <param name="triad1">Первая тройка</param>
+        /// <param name="triad2">Вторая тройка</param>
+        /// <returns>true, если две тройки равны, false, если две тройки не равны</returns>
+        public bool CompareTriad(Triad triad1, Triad triad2)//публичная фукнция с логическим типом данных(true/false) с параметрами triad1, triad2, где triad1, triad2 - обьект класса Triad
+        {
+            return triad1.One == triad2.One && triad1.Two == triad2.Two && triad1.Three == triad2.Three;
+        }
+        /// <summary>
+        /// Сложение тройки на число пользователя
+        /// </summary>
+        /// <param name="userValue">Число пользователя</param>
+        /// <param name="triad1">Тройка чисел</param>
+        /// <returns>Сумма тройки</returns>
+        public Triad SumOfNum(int userValue, Triad triad1)
+        {
+            Triad triad3 = new Triad();
+            triad3.One = triad1.One + userValue;
+            triad3.Two = triad1.Two + userValue;
+            triad3.Three = triad1.Three + userValue;
+            return triad3;
+        }
+        /// <summary>
+        /// Усножение тройки на число пользователя
+        /// </summary>
+        /// <param name="userValue">Число пользователя</param>
+        /// <param name="triad1">Тройка чисел</param>
+        /// <returns>Произведение тройки</returns>
+        public Triad PowOfNum(int userValue, Triad triad1)
+        {
+            Triad triad3 = new Triad();
+            triad3.One = triad1.One * userValue;
+            triad3.Two = triad1.Two * userValue;
+            triad3.Three = triad1.Three * userValue;
+            return triad3;
+        }
+        /// <summary>
+        /// Сложение двух троек
+        /// </summary>
+        /// <param name="triad1">Первая тройка</param>
+        /// <param name="triad2">Вторая тройка</param>
+        /// <returns>Сумму двух троек</returns>
+        public Triad SumOfNum(Triad triad1, Triad triad2)
+        {
+            Triad triad3 = new Triad();
+            triad3.One = triad1.One + triad2.One;
+            triad3.Two = triad1.Two + triad2.Two;
+            triad3.Three = triad1.Three + triad2.Three;
+            return triad3;
+        }
+        // перегружаем бинарный оператор сложения(+) для того, чтобы он мог складывает обьект класса triad1 и целочисленное число которое ввел пользователь
+        public static Triad operator +(Triad triad1, int userValue) /* (public)публичная(доступна из любого места проекта) 
+                                                                     * (static)статическая(не нужно создавать обьект класса Triad чтобы, обращаться к этой фукнции) 
+                                                                     * Triad(это значит что, мы должны вернуть обьект класса Triad) 
+                                                                     * operator(ключевое слово) 
+                                                                     * +(означает какую оператор мы хотим перегрузить)
+                                                                     * С параметрами(triad1, int userValue), где triad1 - обьект класса Triad, int userValue - целочисленное значение которое ввел пользователь
+                                                                     */
+        {
+            Triad triad2 = new Triad();
+            triad2.One = triad1.One + userValue;
+            triad2.Two = triad1.Two + userValue;
+            triad2.Three = triad1.Three + userValue;
+            return triad2;
+        }
+        public static Triad operator +(Triad triad1, Triad triad2) /*публичная(доступна из любого места проекта) 
+                                                                     * статическая(не нужно создавать обьект класса Triad чтобы, обращаться к этой фукнции) 
+                                                                     * Triad(это значит что, мы должны вернуть обьект класса Triad) 
+                                                                     * operator(ключевое слово) 
+                                                                     * +(означает какую оператор мы хотим перегрузить)
+                                                                     * С параметрами(triad1, triad2), где triad1, triad2 - обьект класса Triad
+                                                                     */
+        {
+            Triad triad3 = new Triad(); // создается новый обьект класса Triad
+            triad3.One = triad1.One + triad2.One; // обьект(triad3) со свойством One = обьект который мы получаем из параметра(triad1) со свойством One + обьект который мы получаем из параметра(triad2) со свойством One
+            triad3.Two = triad1.Two + triad2.Two; // обьект(triad3) со свойством One = обьект который мы получаем из параметра(triad1) со свойством Two + обьект который мы получаем из параметра(triad2) со свойством Two
+            triad3.Three = triad1.Three + triad2.Three; // обьект(triad3) со свойством One = обьект который мы получаем из параметра(triad1) со свойством Three + обьект который мы получаем из параметра(triad2) со свойством Three
+            return triad3; // возвращаем обьект(triad3) с новыми значениями в свойствах One, Two, Three
+        }
+    }
+}
